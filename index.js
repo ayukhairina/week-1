b8b80dc1f2717900954e7c6d9e9028de13d4ee8e
ayukhairina <wahyukhairina@gmail.com>
2020-02-14 17:45:42 +0800

    crud

--- index.js
+++ index.js
@@ -2,59 +2,24 @@ const express = require ('express')
 const app = express()
 const logger = require('morgan')
 const bodyParser = require('body-parser')
-const mysql = require ('mysql')
+const mainNavigation = require('./src/routes')
+//const mysql = require ('mysql')
 
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'dev_sample'
-})
+//const connection = mysql.createConnection({
+  //  host     : 'localhost',
+    //user     : 'root',
+    //password : '',
+    //database : 'dev_sample'
+//})
 
-connection.connect((error) => {
-    if(error) console.log(error)
-    console.log('Database connected!')
-})
+//connection.connect((error) => {
+  //  if(error) console.log(error)
+    //console.log('Database connected!')
+//})
 
-app.listen(8001, () => console.log('This server is running')); //service berjalan di port tsb
+app.listen(8002, () => console.log('This server is running')); //service berjalan di port tsb
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extented: false}))
+app.use('/', mainNavigation)
 
-app.get('/', (request, response) => {
-    connection.query('SELECT * FROM book', (error, result) => {
-        if (error) console.log(error)
-        response.json(result)
-    })
-})
-
-app.get('/', (request, response) => {
-   connection.query('INSERT INTO book SET ?', data, (error, result) => {
-       if (error) console.log (error)
-       response.json(result)
-   } )
-   
-    //response.send('Helo Arkademian!')
-    //bentuk lain
-    //response.json({name: 'Wahyu Khairina'})
-})
-
-app.post('/', (request, response) => {
-    //const name = request.body.name
-    const data = {
-        name: request.body.name,
-        writer: request.body.writer,
-        description: request.body.description,
-        publisher: request.body.publisher,
-        year: request.body.year,
-        stock: request.body.stock,
-        genre: request.body.genre,
-        created_at : new Date(),
-        updated_at : new Date()
-    }
-
-    connection.query('INSERT INTO book SET ?', data, (error,result) => {
-        if (error) console.log(error)
-        response.json(result)
-    })
-})
---
+++ src/config/mysql.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'dev_sample'
+})
+
+connection.connect((error) => {
+    if (error) console.log(error)
+    console.log('Database connected')
+})
+
+module.exports = connection
